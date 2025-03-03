1c514af57d9fba03b65da0747ff58c7c01b87dcb
LeoMedeirosss <lmf@cesar.school>
2025-03-03 17:47:29 -0300

    history 1.0

--- Project/Pomodoro/src/pages/History/index.tsx
+++ Project/Pomodoro/src/pages/History/index.tsx
@@ -1,7 +1,53 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
     return(
-        <div>
-            <h1>History</h1>
-        </div>
+        <HistoryContainer>
+            <h1>Historico</h1>
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>Concluído</td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>Concluído</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
     )
 }
