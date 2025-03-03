22927b24561517ef26d9b620f207ddbad1e017ed
LeoMedeirosss <lmf@cesar.school>
2025-03-03 18:40:24 -0300

    history 2.0

--- Project/Pomodoro/src/pages/History/index.tsx
+++ Project/Pomodoro/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
     return(
@@ -11,7 +11,9 @@ export function History() {
                             <th>Tarefa</th>
                             <th>Duração</th>
                             <th>Início</th>
-                            <th>Status</th>
+                            <th>
+
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -19,31 +21,57 @@ export function History() {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 1 mês</td>
-                            <td>Concluído</td>
+                            <td>
+                                <Status $statusColor="yellow">Em andamento</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 1 mês</td>
-                            <td>Concluído</td>
+                            <td>
+                                <Status $statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 1 mês</td>
-                            <td>Concluído</td>
+                            <td>
+                                <Status $statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 1 mês</td>
-                            <td>Concluído</td>
+                            <td>
+                                <Status $statusColor="red">Interrompido</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 1 mês</td>
-                            <td>Concluído</td>
+                            <td>
+                                <Status $statusColor="yellow">Em andamento</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>
+                                <Status $statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 1 mês</td>
+                            <td>
+                                <Status $statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
--- Project/Pomodoro/src/pages/History/styles.ts
+++ Project/Pomodoro/src/pages/History/styles.ts
@@ -2,8 +2,7 @@ import styled from "styled-components"
 
 export const HistoryContainer = styled.main`
     flex:1;
-    padding:3.5rem;
-
+    padding:0.5rem;
     display:flex;
     flex-display:column;
 
@@ -43,24 +42,48 @@ export const HistoryList = styled.div`
         }
                 
 
-            td {
-                background-color: ${(props) => props.theme["gray-700"]};
-                border-top: 3px solid ${(props) => props.theme["gray-800"]};
-                border-left:none;
-                border-right:none;
-                padding: 1rem;
-                font-size: 0.875rem;
-                line-height: 1.6;
+        td {
+            background-color: ${(props) => props.theme["gray-700"]};
+            border-top: 3px solid ${(props) => props.theme["gray-800"]};
+            border-left:none;
+            border-right:none;
+            padding: 1rem;
+            font-size: 0.875rem;
+            line-height: 1.6;
 
-                &:first-child {
-                    width:50%;
-                    padding-left:1.5rem;
-                }
+            &:first-child {
+                width:50%;
+                padding-left:1.5rem;
+            }
             
-                &:last-child {
-                    padding-right:1.5rem;
-                }
+            &:last-child {
+                padding-right:1.5rem;
             }
         }
+    }
+`
+
+const STATUS_COLORS = {
+    yellow: "yellow-500",
+    green: "green-500",
+    red: "red-500",
+    gray: "gray-500"
+} as const
+
+interface StatusProps {
+    $statusColor: keyof typeof STATUS_COLORS;
+}
+
+export const Status = styled.span<StatusProps>`
+    display:flex;
+    align-items: center;
+    gap:0.5rem;
+
+    &::before {
+        content: "";
+        width: 0.5rem;
+        height:0.5rem;
+        border-radius: 50%;
+         background: ${(props) => {return props.theme[STATUS_COLORS[props.$statusColor]]}};
     }
 `
